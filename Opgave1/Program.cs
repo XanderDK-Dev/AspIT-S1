882ece64fbc64d946af1919edb6ef6dd14f1b801
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-20 13:57:29 +0100

    Worked on 2 more tasks

---
+++ Opgave1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Opgave1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast en af følgene dyr:\n" +
+                "Gris, Hund, Kat eller Ko:");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "gris":
+                    Console.WriteLine("Grisen har en grisehale og leverer bacon.");
+                    break;
+                case "hund":
+                    Console.WriteLine("Hunde kan lygte mindst 40 gange bedre end os");
+                    break;
+                case "kat":
+                    Console.WriteLine("Katte kan hoppe 5 gange deres højde");
+                    break;
+                case "Ko":
+                    Console.WriteLine("Køer bliver normalt op til 20 år");
+                    break;
+                default: Console.WriteLine("Error...");
+                    break;
+            }
+        }
+    }
+}
---
+++ Opgave2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Opgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime thisDate1 = new DateTime(2011, 6, 10);
+            Console.WriteLine("Today is " + thisDate1.ToString("MMMM dd, yyyy") + ".");
+
+            DateTime today = DateTime.Today;
+            DateTime.Now.ToString("M/d/yyyy");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(today);
+            string dayOfWeek = today.DayOfWeek.ToString("d");
+            Console.WriteLine(dayOfWeek);
+
+        }
+    }
+}
