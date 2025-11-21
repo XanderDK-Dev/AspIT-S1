75d2ff7554bb630e73b69837c80684b75bb51755
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-21 11:36:32 +0100

    First 3 of 5 "Tasks"

---
+++ Opgave2/Opgave2Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Opgave2
+{
+    internal class Opgave2Program
+    {
+        static void Main(string[] args)
+        {
+            var date = DateTime.Now.ToString("dd/MM/yyyy");
+            var day = DateTime.Today.DayOfWeek;
+            Console.WriteLine($"Dags dato: {date}");
+            Console.WriteLine($"Dagen i dag er {day}.");
+
+        }
+    }
+}
---
+++ Opgave3/Opgave3Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Opgave3
+{
+    internal class Opgave3Program
+    {
+        static void Main(string[] args)
+        {
+            string tryAgain = "1";
+            while (tryAgain == "1")
+            {
+                Console.Clear();
+                Random rnd = new Random();
+                int dice = rnd.Next(1, 7);
+                string result = "";
+                switch (dice)
+                {
+                    case 1:
+                        result = "etter"; break;
+                    case 2:
+                        result = "toer"; break;
+                    case 3:
+                        result = "treer"; break;
+                    case 4:
+                        result = "fire"; break;
+                    case 5:
+                        result = "femmer"; break;
+                    case 6:
+                        result = "sekser"; break;
+                    default:
+                        break;
+                }
+                Console.WriteLine($"Du har slået en {result}!");
+                Console.WriteLine("1) Prøv igen\n2) Afslut");
+                tryAgain = Console.ReadLine();
+            }
+                    
+        }
+    }
+}
