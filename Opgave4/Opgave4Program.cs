1f59b59356d411f90aff1769767321fcf62b5770
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-21 11:37:26 +0100

    "Task" 4 and 5 and also the updated solution

---
+++ Opgave4/Opgave4Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Opgave4
+{
+    internal class Opgave4Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast et familiemedlem (fx mor, bedstefar eller lignende");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "mor":
+                case "far":
+                    Console.WriteLine("Du har indtastet et " +
+                        "2. generationsmedlem af familien.");
+                    break;
+                case "mormor":
+                case "farmor":
+                case "farfar":
+                    Console.WriteLine("Du har indtastet et " +
+                        "3. generationsmedlem af familien.");
+                    break;
+                case "bedstemor":
+                case "bedstefar":
+                    Console.WriteLine("Du har indtastet et " +
+                        "4. generationsmedlem af familien.");
+                    break;
+                default:
+                    Console.WriteLine("Du har indtastet et ukendt familiemedlem");
+                    break;
+            }
+        }
+    }
+}
---
+++ Opgave5/Opgave5Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Opgave5
+{
+    internal class Opgave5Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tryk på en vilkårlig tast");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.B:
+                case ConsoleKey.D:
+                case ConsoleKey.F:
+                case ConsoleKey.G:
+                case ConsoleKey.H:
+                case ConsoleKey.J:
+                case ConsoleKey.K:
+                case ConsoleKey.L:
+                case ConsoleKey.M:
+                case ConsoleKey.N:
+                case ConsoleKey.P:
+                case ConsoleKey.Q:
+                case ConsoleKey.R:
+                case ConsoleKey.S:
+                case ConsoleKey.T:
+                case ConsoleKey.V:
+                case ConsoleKey.X:
+                case ConsoleKey.Z:
+                    Console.WriteLine("Du trykkede på en konsonant");
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.E:
+                case ConsoleKey.I:
+                case ConsoleKey.O:
+                case ConsoleKey.U:
+                case ConsoleKey.Y:
+                    Console.WriteLine("Du trykkede på en vokal");
+                    break;
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Ukendt tastetryk");
+                    break;
+            }
+        }
+    }
+}
