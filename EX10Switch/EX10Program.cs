753ff8b53837ab1bd96936330e7c5ed8c4e04e3e
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-21 11:35:41 +0100

    Made EX10 and EX11

---
+++ EX10Switch/EX10Program.cs
@@ -0,0 +1,82 @@
+﻿namespace EX10Switch
+{
+    internal class EX10Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a) Kæledyrsopslagsværk\nb) Karakterbeskrivelsen");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.A:
+                        Console.WriteLine("Indtast en af følgene dyr:\n" +
+                            "Gris, Hund, Kat Undulat eller Papegøje:");
+                        string input = Console.ReadLine();
+                        switch (input.ToLower())
+                        {
+                            case "gris":
+                                Console.WriteLine("Grisen har en grisehale og leverer bacon.");
+                                break;
+                            case "hund":
+                                Console.WriteLine("Hunde kan lygte mindst 40 gange bedre end os");
+                                break;
+                            case "kat":
+                                Console.WriteLine("Katte kan hoppe 5 gange deres højde");
+                                break;
+                            case "undulat":
+                                Console.WriteLine("En undulat kan mimic meneske snak");
+                                break;
+                            case "papegøje":
+                                Console.WriteLine("Papegøjer kan leve i årtier");
+                                break;
+                            default:
+                                Console.WriteLine("Error...");
+                                break;
+                        }
+                        break;
+
+                case ConsoleKey.B:
+                    Console.WriteLine("Indtast en af følgene karaktere:\n" +
+                            "-3, 00, 02, 4, 7, 10 eller 12::");
+                    string grade = Console.ReadLine();
+                    switch (grade.ToLower())
+                    {
+                        case "-3":
+                            Console.WriteLine("For den ringe præstation. Karakteren -3 gives for den helt uacceptable præstation.");
+                            break;
+                        case "00":
+                            Console.WriteLine("For den utilstrækkelige præstation. Karakteren 00 gives for den utilstrækkelige præstation, der ikke demonstrerer en acceptabel grad af opfyldelse af fagets/fagelementets mål.");
+                            break;
+                        case "02":
+                            Console.WriteLine("For den tilstrækkelige præstation. Karakteren 02 gives for den tilstrækkelige præstation, der demonstrerer den minimalt acceptable grad af opfyldelse af fagets/fagelementets mål.");
+                            break;
+                        case "4":
+                            Console.WriteLine("For den jævne præstation. Karakteren 4 gives for den jævne præstation, der demonstrerer en mindre grad af opfyldelse af fagets/fagelementets mål, med adskillige væsentlige mangler.");
+                            break;
+                        case "7":
+                            Console.WriteLine("For den gode præstation. Karakteren 7 gives for den gode præstation, der demonstrerer opfyldelse af fagets/fagelementets mål, med en del mangler.");
+                            break;
+                        case "10":
+                            Console.WriteLine("For den fortrinlige præstation. Karakteren 10 gives for den fortrinlige præstation, der demonstrerer omfattende opfyldelse af fagets/fagelementets mål, med nogle mindre væsentlige mangler.");
+                            break;
+                        case "12":
+                            Console.WriteLine("For den fremragende præstation. Karakteren 12 gives for den fremragende præstation, der demonstrerer udtømmende opfyldelse af fagets/fagelementets mål, med ingen eller få uvæsentlige mangler.");
+                            break;
+                        default:
+                            Console.WriteLine("Error...");
+                            break;
+                    }
+                    break;
+               
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Ukendt tastetryk");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ EX11While/EX11Program.cs
@@ -0,0 +1,92 @@
+﻿namespace EX11While
+{
+    internal class EX11Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a) Udskriv talrækker\nb) Udskriv tabeller\nc) Få det rette input");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.A:
+                    int x = 0;
+                    while (x < 10)
+                    {
+                        x++;
+                        Console.WriteLine(x);
+                    }
+                    Console.WriteLine("----");
+                    int y = 11;
+                    while (y > 1)
+                    {
+                        y--;
+                        Console.WriteLine(y);
+                    }
+                    Console.WriteLine("----");
+                    int z = 0;
+                    while (z < 20)
+                    {
+                        z++;
+                        if (z % 2 == 0)
+                        {
+                            Console.WriteLine(z);
+                        }
+
+                    }
+                    Console.WriteLine("----");
+                    int a = 0;
+                    while (a < 20)
+                    {
+                        a++;
+                        if (a % 2 != 0)
+                        {
+                            Console.WriteLine(a);
+                        }
+
+                    }
+                    Console.WriteLine("----");
+                    break;
+
+                case ConsoleKey.B:
+                    int num1 = 0;
+                    int num2 = 0;
+                    int time = 0;
+                    Console.WriteLine("Indtast en tabel (fx 7, 13 eller lignende");
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    while (time < 10)
+                    {
+                        time = time + 1;
+                        num2 = num1 + num2;
+                        Console.WriteLine(num2);
+
+                    }
+                    break;
+                
+                case ConsoleKey.C:
+                    int tryAgain = 1;
+                    int number = 0;
+                    while (tryAgain == 1)
+                    {
+                        Console.WriteLine("Indtast et tal mellem 1 og 10.");
+                        number = int.Parse(Console.ReadLine());
+                        if (number >= 1 && number <= 10)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Well done!");
+                            tryAgain = 0;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Number was not between 1 and 10");
+                        }
+
+                    }
+                    break;
+            }
+
+        }
+    }
+}
