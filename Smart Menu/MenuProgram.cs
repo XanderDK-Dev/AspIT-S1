2a471d459d9df4ef412472161d3cc8a8f6707da6
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-24 14:35:16 +0100

    Made a task and a menu

---
+++ Smart Menu/MenuProgram.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Smart_Menu
+{
+    internal class MenuProgram
+    {
+        static void Main(string[] args)
+        {
+            string[] options = { "Artemis", "Project hail mary", "The martian", "bye bye" };
+            int selection = 0;
+            bool isRun = true;
+
+            while (isRun)
+            {
+                Console.Clear();
+                if (selection == 0)
+                {
+                    Console.WriteLine($"*{options[0]}*");
+                }
+                else
+                {
+                    Console.WriteLine(options[0]);
+                }
+                if (selection == 1)
+                {
+                    Console.WriteLine($"*{options[1]}*");
+                }
+                else
+                {
+                    Console.WriteLine(options[1]);
+                }
+                if (selection == 2)
+                {
+                    Console.WriteLine($"*{options[2]}*");
+                }
+                else
+                {
+                    Console.WriteLine(options[2]);
+                }
+                if (selection == 3)
+                {
+                    Console.WriteLine($"*{options[3]}*");
+                }
+                else
+                {
+                    Console.WriteLine(options[3]);
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key) 
+                {
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.W:
+                        selection--;
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.S:
+                        selection++;
+                        break;
+                }
+                if (selection > 3)
+                {
+                    selection = 0;
+                }
+
+                if (selection < 0)
+                {
+                    selection = 3;
+                }
+                if (selection == 3 && keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Environment.Exit(0);
+                }
+                if (selection == 0 && keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Artemis is a 2017 science fiction novel by American writer Andy Weir.\n It takes place in the late 2080s in Artemis, the first and so far only city on the Moon\n. It follows the life of porter and smuggler Jasmine \"Jazz\" Bashara as she gets caught up\n in a conspiracy for control of the city.");
+                }
+            }
+        }
+    }
+}
---
+++ WhileOpgave1/WhileOpgave1Program.cs
@@ -0,0 +1,40 @@
+﻿namespace WhileOpgave1
+{
+    internal class WhileOpgave1Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a) Opgave1\nb) Udskriv tabeller\nc) Få det rette input");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.A:
+                    int x = -1;
+                    while (x < 9)
+                    {
+                        x++;
+                        Console.WriteLine($"While statement {x}");
+                    }
+                    Console.WriteLine("Denne løkke har kørt 10 gange.");
+                    break;
+                case ConsoleKey.B:
+                    Console.WriteLine("1st number");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("2nd Number");
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("3rd Number");
+                    int num3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("4th Number");
+                    int num4 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("5th Number");
+                    int num5 = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    int finalArea = num1 + num2 + num3 + num4 + num5;
+                    Console.WriteLine(finalArea);
+                    break;
+            }
+        }
+    }
+}
