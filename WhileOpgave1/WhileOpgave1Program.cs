b778b379c342b5410783c4dfa5f8ec37488e1502
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-11-26 07:19:08 +0100

    Worked a little on the While Tasks

--- WhileOpgave1/WhileOpgave1Program.cs
+++ WhileOpgave1/WhileOpgave1Program.cs
@@ -34,6 +34,47 @@
                     int finalArea = num1 + num2 + num3 + num4 + num5;
                     Console.WriteLine(finalArea);
                     break;
+
+                case ConsoleKey.C:
+                    string input1 = "";
+                    Console.WriteLine("Skriv noget tekst:");
+                    input1 = Console.ReadLine();
+                    char[] chars = input1.ToCharArray();
+                    foreach (char c in chars)
+                        Console.WriteLine(c);
+                    break;
+               
+                case ConsoleKey.D:
+                    string input2 = "";
+                    while (input2.ToLower() != "exit")
+                    {
+                        Console.WriteLine("Skriv 'exit' for at afslutte:");
+                        input2 = Console.ReadLine();
+                        Console.WriteLine($"Du skrev: {input2.ToLower()}");
+                    }
+                    break;
+                
+                case ConsoleKey.E:
+                    while (true)
+                    {
+                        Console.WriteLine("Skriv exit for at stoppe");
+                        string input = Console.ReadLine();
+                        if (input == "exit")
+                        {
+                            break;
+                        }
+                    }
+                    break;
+
+                case ConsoleKey.F:
+                    int num = 15;
+                    while (num < 0 || num > 11)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Indtast et tal der er større end 0 og mindre end 11");
+                        num = int.Parse(Console.ReadLine());
+                    }
+                    break;
             }
         }
     }
