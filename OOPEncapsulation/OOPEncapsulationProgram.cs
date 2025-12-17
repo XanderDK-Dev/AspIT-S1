4e831d7b772b4af8f04b82cf6a576d05cbebf5b2
XanderDK-Dev <124628870+XanderDK-Dev@users.noreply.github.com>
2025-12-17 08:35:33 +0100

    Add files via upload

---
+++ OOPEncapsulation/OOPEncapsulationProgram.cs
@@ -0,0 +1,25 @@
+﻿namespace OOPEncapsulation
+{
+
+    internal class OOPEncapsulationProgram
+    {
+        static void Main(string[] args)
+        {
+            Person p = new();
+            try
+            {
+                p.FirstName = " ";
+                p.LastName = "Hansen";
+                p.BirthDate = new DateTime(1946, 01, 02);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            string output = p.GetFullName();
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ OOPEncapsulation/Person.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OOPEncapsulation
+{
+    internal class Person
+    {
+        private string name;
+        public string Name 
+        {
+            get 
+            { 
+                return name;
+            }
+            set 
+            { 
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Not allowed as name.");
+                }
+                name = value;
+            }
+        }
+
+
+
+
+
+
+
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
